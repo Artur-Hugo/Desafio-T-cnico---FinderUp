efcdff8b32832537809421eee293e31a8e3ae441
Artur-Hugo <Artur.Hugoliga@gmail.com>
2021-12-29 21:35:42 -0300

    Iniciando o projeto

---
+++ src/main/java/com/oliga/fabricacaodebolo/FabricBoloApplication.java
@@ -0,0 +1,13 @@
+package com.oliga.fabricacaodebolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricBoloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabricBoloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oliga/fabricacaodebolo/FabricBoloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliga.fabricacaodebolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabricBoloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
